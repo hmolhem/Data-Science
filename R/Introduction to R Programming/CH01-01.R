ca39f23e9151863934e6fdbf8bb5fd93d5e7cfd2
hmolhem <hosseinmolhem@gmail.com>
2022-04-17 01:03:34 +0430

    CH01-01: get and change current workspace & define variable in R

---
+++ R/Introduction to R Programming/CH01-01.R
@@ -0,0 +1,10 @@
+getwd() # get current workspace
+setwd("D:/Hossein/My GitHub Web Design/DataScience") # set workspace
+
+# define variable in R
+x = 10;
+y <- 20;
+print(x) # print value on console
+name <- "Hossein"
+print(name)
+flag = TRUE
