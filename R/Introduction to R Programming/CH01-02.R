12c4504fbe77b426aef0f671e2fa6101c5c5c53c
hmolhem <hosseinmolhem@gmail.com>
2022-04-17 01:07:32 +0430

    CH01-02: class or data object type and structure in R

---
+++ R/Introduction to R Programming/CH01-02.R
@@ -0,0 +1,13 @@
+# all class in R
+# numeric, logical, character,
+class(x) # numeric
+class(name) # character
+class(flag) # logical
+
+# change class of variable
+x <- as.integer(x)
+class(x) # integer
+x <- as.character(x)
+class(x) # character
+x <- as.logical(x)
+class(x) # logical
