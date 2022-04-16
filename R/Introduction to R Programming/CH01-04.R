1f4b318640e1f062842f3c48977149997e4f6ecd
hmolhem <hosseinmolhem@gmail.com>
2022-04-17 01:11:45 +0430

    CH01-04: some mathematical function in R

---
+++ R/Introduction to R Programming/CH01-04.R
@@ -0,0 +1,26 @@
+getwd() # get current workspace
+setwd("D:/Hossein/My GitHub Web Design/DataScience/R/Introduction to Data Analytics with R") # set workspace
+
+# some mathematical function in R
+
+x = -23.45671
+y = 5.143089
+abs(x) # 23.45671
+sign(x) # -1
+sign(y) # 1
+sqrt(25) # 5
+round(y,3) # 5.143
+exp(10) # 22026.47
+log(100,10) # 2
+log10(100) # 2
+log(100,2) # 6.643856
+log1p(100) # 4.615121
+logb(10,2) # 3.321928
+pi # 3.141593
+sin(2*pi) # -2.449213e-16
+cos(2*pi/3) # -0.5
+x = seq(1:5) # 1 2 3 4 5
+x
+cumsum(x) # 1  3  6 10 15
+cumprod(x) # 1   2   6  24 120
+factorial(4) # 24
