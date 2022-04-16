1ba641a5065bef066a7b22d6bf71c8071f453605
hmolhem <hosseinmolhem@gmail.com>
2022-04-17 01:13:22 +0430

    CH01-05: some statistical function in R

---
+++ R/Introduction to R Programming/CH01-05.R
@@ -0,0 +1,14 @@
+getwd() # get current workspace
+setwd("D:/Hossein/My GitHub Web Design/DataScience/R/Introduction to Data Analytics with R") # set workspace
+
+# some statistical function in R
+
+x <- c(1,4,5,7,9,13)
+y <- c(10, 37, 53, 68, 81, 118)
+min(x) # 1
+max(y) # 118
+mean(x) # 6.5
+var(y) # 1387.767
+sd(y) # 37.25274
+cor(x,y) # 0.9965388
+
