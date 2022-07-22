b7cd7e8da94d6e865903da14461959e7d10961bd
hmolhem <hosseinmolhem@gmail.com>
2022-07-22 18:33:33 +0430

    Exercise Chapter 03

---
+++ Python/py4e/CH03-Coditional execution/EX_03_03.py
@@ -0,0 +1,18 @@
+try:
+    score = float(input('Enter Hours: '))
+    flag = score >= 0 and score <= 1
+except:
+    print('Bad score')
+    quit()
+
+if score >= 0.9 and score <= 1:
+    print('Grade: A')
+elif score >= 0.8 and score < 0.9:
+    print('Grade: B')
+elif score >= 0.7 and score < 0.8:
+    print('Grade: C')
+elif score >= 0.6 and score < 0.7:
+    print('Grade: D')
+elif score >= 0 and score < 0.6:
+    print('Grade: F')
+else: print('Bad score')
